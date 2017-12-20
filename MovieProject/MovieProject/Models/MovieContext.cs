60aad4770d05a311a06b9eefffb8380e7be63f65
Wildpuff <nattemann@hotmail.com>
2017-12-20 16:15:58 +0100

    MovieContext and MovieInitializer(not finished)

---
+++ MovieProject/MovieProject/Models/MovieContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MovieProject.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext() : base("MovieConnection")
+        {
+
+        }
+
+        DbSet<Movie> Movies { get; set; }
+        DbSet<Customer> Customers { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<OrderRow> OrderRows { get; set; }
+    }
+}
---
+++ MovieProject/MovieProject/Models/MovieInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MovieProject.Models
+{
+    public class MovieInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<MovieContext>
+    {
+        protected override void Seed(MovieContext context)
+        {
+            var customers = new Customer
+            {
+                FirstName = "Jonas",
+                LastName = "Gray",
+                BillingAddress = "23 Green Corner Street",
+
+                
+            };
+            //base.Seed(context);
+        }
+    }
+}
