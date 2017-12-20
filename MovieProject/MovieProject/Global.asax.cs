be9c2789cd9dc29b8e0911dfa2c5bb085c5389e5
Roflcoffel <roflcoffel@gmail.com>
2017-12-20 13:54:12 +0100

    Setup Complete; Next: Code First DB

--- MovieProject/MovieProject/Global.asax.cs
+++ MovieProject/MovieProject/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MovieStore.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MovieProject
@@ -13,6 +15,7 @@ namespace MovieProject
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
