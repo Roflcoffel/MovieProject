8cacfd7a1b72b062eb40e9dba0ca67aa78286693
TonyHomsi <tonyhomsi87@gmail.com>
2017-12-21 13:07:51 +0100

    failed Result

--- MovieProject/MovieProject/Models/User.cs
+++ MovieProject/MovieProject/Models/User.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MovieProject.Models
 {
     public class User
     {
+        [Key, ForeignKey("Customer")]
         [Required]
         public int Id { get; set; }
         [Required]
@@ -17,8 +19,8 @@ namespace MovieProject.Models
         [StringLength(30)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-        [Required]
-        public bool Admin { get; set; }
+        
+        //public bool Admin { get; set; }
         public int CustomerId { get; set; }
 
         public virtual Customer Customer { get; set; }
