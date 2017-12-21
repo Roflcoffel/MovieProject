d2c569ba2a0fe3455b3f1423d6c7e182bf5fb935
TonyHomsi <tonyhomsi87@gmail.com>
2017-12-21 16:02:53 +0100

    LastAttempttouploadImage

--- MovieProject/MovieProject/Migrations/201712211414145_init.cs
+++ MovieProject/MovieProject/Migrations/201712211414145_init.cs
@@ -3,7 +3,7 @@ namespace MovieProject.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class MovieUpdate : DbMigration
+    public partial class init : DbMigration
     {
         public override void Up()
         {
