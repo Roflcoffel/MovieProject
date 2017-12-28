bbe23336714f66d75ef270e651877575cc4fb1f5
Roflcoffel <roflcoffel@gmail.com>
2017-12-28 13:42:39 +0100

    CheckOutVM

---
+++ MovieProject/MovieProject/Models/CheckOutVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieProject.Models {
+    public class CheckOutVM {
+
+        public Customer Customer { get; set; }
+        public List<Movie> ShoppingCart { get; set; }
+        public int TotalCost { get; set; }
+
+    }
+}
