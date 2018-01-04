f575eb43293972ec8bc09d74e894d4ce6dd6f64e
Wildpuff <nattemann@hotmail.com>
2018-01-04 12:53:32 +0100

    review

---
+++ MovieProject/MovieProject/Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieProject.Models
+{
+    public class Review
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int MovieId { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public int Rating { get; set; }
+
+        public virtual Movie Movie { get; set; }
+        public virtual Customer Customer { get; set; }
+    }       
+}
